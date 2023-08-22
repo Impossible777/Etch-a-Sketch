3dfcc54f7dc927e26d395ad602399eefdb710e7c
Impossible777 <cristmichael7@gmail.com>
2023-08-22 17:05:15 -0500

    Added for loop for grid row creation.

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const container = document.querySelector('#container');
+for (let i = 0; i <16; i++) {
+    const content = document.createElement('div');
+    content.classList.add('boxes')
+    container.appendChild(content);
+
+
+}
