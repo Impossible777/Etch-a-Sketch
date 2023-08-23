6e6d570d21fc48115b7f3713ee1684d4b9c9e3d3
Impossible777 <cristmichael7@gmail.com>
2023-08-23 18:29:14 -0500

    Fixed issue with overall grid size changing when the size of the squares are changed.

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+
+let gridSize = 16
+let totalGrid = gridSize * gridSize
+let boxHeight = (500 / gridSize);
+let boxWidth =  (500/gridSize);
 const container = document.querySelector('#container');
-for (let i = 0; i <16; i++) {
-    const minorContent = document.createElement('div')
-    const minorContentId = 'minorContainer' + i;
-    minorContent.setAttribute('id', minorContentId)
-    container.appendChild(minorContent);
-        for (let j=0; j<16; j++) {
-            const content = document.createElement('div');
-            content.classList.add('boxes')
-            minorContent.appendChild(content);
-        }
+for (let i = 0; i < totalGrid; i++) {
+    const content = document.createElement('div');
+    content.classList.add('boxes')
+    container.appendChild(content);
+    content.style.height = boxHeight + 'px';
+    content.style.width = boxWidth + 'px'
 }
 
 const hovers = document.querySelectorAll('.boxes')
@@ -22,9 +23,57 @@ hovers.forEach(hover =>{
 })
 
 function transitionYellow () {
+    if (mouseIsDown) {
     this.classList.add('hovered')
+    }
 }
 
 function removeYellow () {
     this.classList.remove('hovered')
+}
+
+var mouseIsDown = false;
+document.addEventListener('mousedown', function(){mouseIsDown = true});
+document.addEventListener('mouseup', function(){mouseIsDown = false});
+
+const changeGrid = document.getElementById("button")
+
+changeGrid.addEventListener("click", function(){
+    let userInput = prompt("How many squares would you like per side? 100 is the max.")
+    if (userInput > 100) {
+        userInput = 100;
+    }
+    let gridSize = parseInt(userInput);
+    removeAllDivs();
+    let totalGrid = gridSize * gridSize
+    let boxHeight = (500 / gridSize);
+    let boxWidth =  (500/gridSize);
+    const container = document.querySelector('#container');
+    for (let i = 0; i < totalGrid; i++) {
+        const content = document.createElement('div');
+        content.classList.add('boxes')
+        container.appendChild(content);
+        content.style.height = boxHeight + 'px';
+        content.style.width = boxWidth + 'px'
+
+
+
+
+        content.addEventListener('mouseover', transitionYellow);
+        content.addEventListener('click', removeYellow);
+    }            
+})
+
+function removeAllDivs() {
+    container.innerHTML = '';
+}
+
+function createGrid() {
+    for (let i = 0; i < totalGrid; i++) {
+        const content = document.createElement('div');
+        content.classList.add('boxes')
+        container.appendChild(content);
+        content.style.height = boxHeight + 'px';
+        content.style.width = boxWidth + 'px'
+    }
 }
